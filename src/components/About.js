81f14d2c1d91f836965212cf56020137b9250d57
Olha Gruzglina <olga.zhmura@gmail.com>
2022-01-30 17:12:55 -0500

    commit tree f9fded203f346731c98b9434e151c697477493a8

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,19 @@
 import React from "react";
 import { image } from "../data/data";
 
+// a <div> element with an id of "about"
+// inside the div element, there should be three child elements:
+// an <h2> element with the content of "About Me"
+// a <p> element with any content of your choosing
+// an <img> element with the src attribute set to the imported image variable, and the alt attribute set to "I made this"
+
 function About() {
-  return <div>About</div>;
+  return (
+    <div id = "about">
+      <h2>About Me</h2>
+      <p></p>
+      <img src = {image} alt = "I made this" />
+    </div>);
 }
 
 export default About;
